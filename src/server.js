1210be197d238f5c3fd8b26ae850042ce718fc9a
Matheus-Gr <matheus.gr@aluno.ufop.edu.br>
2020-08-06 13:01:15 -0300

    aula 4.3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,60 @@
-require('express')()
-.get("/", (req, res) => {
-    return res.send("HI from nlw")
+const proffys = [
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9999999999",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "9999999999",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "9999999999",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys } )
+}
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
-.get("/study", (req,res) => {
-    return res.send("Pagina study")
-})
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500)
